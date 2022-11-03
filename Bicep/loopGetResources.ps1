f8443e3e8da2a61c21f4b11e9625a4aff6866387
ReneSkovSvendsen <rene@skov-svendsen.net>
2022-11-03 23:03:50 +0100

    sdf

--- Bicep/loopGetResources.ps1
+++ Bicep/loopGetResources.ps1
@@ -2,6 +2,7 @@ $resource = Get-AzResource
 Clear-Host
 while (($resource | Measure-Object).Count -ne 0) {
     Clear-Host
+    Get-Date
     $resource | Format-Table Name, ResourceGroup
     Start-Sleep -Seconds 2
 }
