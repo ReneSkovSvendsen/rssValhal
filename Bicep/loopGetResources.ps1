784eafdef7e21031f9de205e067c838cdb20b9ee
ReneSkovSvendsen <rene@skov-svendsen.net>
2022-11-06 16:12:51 +0100

    kjh

--- Bicep/loopGetResources.ps1
+++ Bicep/loopGetResources.ps1
@@ -1,9 +1,12 @@
 $resource = Get-AzResource
 Clear-Host
+$startdate = Get-Date
 while (($resource | Measure-Object).Count -ne 0) {
-    Clear-Host
-    Get-Date
     $resource = Get-AzResource
+    Clear-Host
+    Write-Host "Script stated: " $startdate
+    Write-Host "Script running: " (Get-Date)
+    Write-Host "Number of ressources: " ($resource | Measure-Object).Count
     $resource | Format-Table Name, ResourceGroupName
     Start-Sleep -Seconds 5
 }
